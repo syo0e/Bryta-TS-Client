b435284ebab59c68d2a8b0761198914b2cb7ea43
kimdlzp <kimdlzp@gmail.com>
2022-01-06 14:57:53 +0900

    apply lint

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import Title from 'components/home/Title';
+import React from 'react';
 
 function Home() {
   return (
-   <>
-    <h1>Home</h1>
-    <div>안녕하세요</div>
-  </>
-  )
-} 
+    <>
+      <Title />
+      <div>안녕하세요.</div>
+    </>
+  );
+}
 
 export default Home;
