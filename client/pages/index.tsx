18916f08c5f42d47193803de2d0f232135fa70d5
kimdlzp <kimdlzp@gmail.com>
2022-01-07 13:33:30 +0900

    index.tsx에 헤더 추가

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,11 @@
 import Title from 'components/home/Title';
 import React from 'react';
+import Header from '../components/common/header';
 
 function Home() {
   return (
     <>
+      <Header />
       <Title />
       <div>안녕하세요.</div>
     </>
