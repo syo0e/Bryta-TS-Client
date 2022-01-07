b1fbb3795b29806ef9610289e4f3f530c435e2e1
kimdlzp <kimdlzp@gmail.com>
2022-01-07 14:02:29 +0900

    헤더 스타일 추가

--- client/components/common/header.tsx
+++ client/components/common/header.tsx
@@ -1,4 +1,4 @@
-import Styled from '@emotion/styled';
+import styled from '@emotion/styled';
 import React from 'react';
 
 function header() {
@@ -25,29 +25,73 @@ function header() {
   );
 }
 
-const HeaderWrap = Styled.div`
-  .logo {
+const HeaderWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 90px;
 
+
+  .logo {
+    margin-left: 20px;
+    width: 450px;
+    height: 700px;
   }
   .user {
     &__anonymous {
+      display: flex;
+      margin-right: 140px;
 
       &--login {
-
+        line-height: 27px;
+        white-space: nowrap;
+        color: #9d9d9d;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
       }
       &--signUp {
-
+        margin-left: 40px;
+        line-height: 27px;
+        white-space: nowrap;
+        color: #ffab53;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
       }
     }
 
     &__profile {
+      display: flex;
+      margin-right: 55px;
+
+      &--logout {
+        margin-right: 40px;
+        line-height: 27px;
+        white-space: nowrap;
+        color: #9d9d9d;
+        font-size:20px;
+        font-weight: bold;
+        font-style: normal;
+      }
 
       &--picture{
-
+        border-radius: 14px;
+        box-shadow: 3px 3px 7px 4px rgba(98, 98, 98, 0.12);
+        background-color: #ffab53;
+        width: 28px;
+        height: 28px;
+        overflow: hidden;
       }
 
       &--name {
-
+        margin-left: 9px;
+        line-height: 27px;
+        white-space: nowrap;
+        color: #ffab53;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
       }
     }
   }
